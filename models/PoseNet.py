4075377c6406418bb946a1aa505ad106895afcf1
Zhiwei Huang <zhiwei.denny@foxmail.com>
2022-05-30 16:12:25 -0700

    add posenet

---
+++ models/PoseNet.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+
+from convnext import convnext_tiny, convnext_small
+
+class PoseDecoder(nn.Module):
+    def __init__(self, num_frames_to_predict_for=1):
+        super(PoseDecoder, self).__init__()
+
+        self.num_frames_to_predict_for = num_frames_to_predict_for
+        self.squeeze = nn.Conv2d(768, 512, 1)
+        self.pose_0 = nn.Conv2d(512, 512, 3, 1, 1)
+        self.pose_1 = nn.Conv2d(512, 512, 3, 1, 1)
+        self.pose_2 = nn.Conv2d(512, 6 * num_frames_to_predict_for, 1)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, input_features):
+        last_features = [f[-1] for f in input_features]
+        x = [self.relu(self.squeeze(f)) for f in last_features]
+        x = torch.cat(x, 1)
+
+        x = self.relu(self.pose_0(x))
+        x = self.relu(self.pose_1(x))
+        x = self.pose_2(x)
+
+        x = x.mean(3).mean(2)
+        pose = 0.01 * x.view(-1, 6)
+
+        return pose
+
+class PoseNet(nn.Module):
+
+    def __init__(self, pretrained = True):
+        super(PoseNet, self).__init__()
+        self.encoder = convnext_small(num_input_images = 2, pretrained=True)
+        self.decoder = PoseDecoder()
+
+    def forward(self, img1, img2):
+        x = torch.cat([img1, img2], 1)
+        features = self.encoder(x)
+        pose = self.decoder([features])
+        return pose
+if __name__ == "__main__":
+
+    torch.backends.cudnn.benchmark = True
+
+    model = PoseNet().cuda()
+    model.train()
+
+    tgt_img = torch.randn(4, 3, 256, 832).cuda()
+    ref_imgs = [torch.randn(4, 3, 256, 832).cuda() for i in range(2)]
+
+    pose = model(tgt_img, ref_imgs[0])
+
+    print(pose.size())
--- models/convnext.py
+++ models/convnext.py
@@ -171,7 +171,7 @@ model_urls = {
 
 @register_model
 def convnext_tiny(num_input_images=1, pretrained=False, in_22k=False, **kwargs):
-    model = ConvNeXt(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs)
+    model = ConvNeXt(in_chans = num_input_images * 3, depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs)
     if pretrained:
         url = model_urls['convnext_tiny_22k'] if in_22k else model_urls['convnext_tiny_1k']
         checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
